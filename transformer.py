ae47067b7e4e59ad1fc6e48acaa3fdb27c1e7322
rainbow <981512226@qq.com>
2024-05-09 17:42:25 +0800

    单block的自注意力Transformer实现

--- transformer.py
+++ transformer.py
@@ -9,14 +9,15 @@ torch.manual_seed(1337)
 if device == "cuda":
     torch.cuda.manual_seed(47)
 train_data_proportion = 0.9
-batch_size = 1024
+batch_size = 32
 block_size = 8
-iterations = 7000
-eval_interval = 300
+iterations = 5000
+eval_interval = 500
 eval_iters = 200
 max_tokens = 500
 # 超参数设置
 learning_rate = 1e-3
+n_blocks = 1
 # ------------------------------------------------------
 
 # 加载数据集
@@ -64,20 +65,90 @@ def estimate_loss():
     return out
 
 
+class AttentionHead(nn.Module):
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        # 解码时，block_size是t的上限，不要误以为相等。预测其实相当于解码。
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+    def forward(self, X):  # (B, T, C) -> (B, T, head_size) 因此只要单头时，head_size必须=C否则无法输入lm_head
+        """
+        :param X: 单个头的输入X(b, t, c)  所有注意力头都拿到完整的真实输入X，不是平分！
+        :return: 单个头的输出OUT(b, t, head_size)  所有注意力头拼接真实输出OUT: head_size = c//head_nums
+        """
+        b, t, c = X.shape  # 无论单头还是多头都接受完整的X输入，这样多头才有明显的优势，如果不是完整的输入，多头就退化成单头的批处理模式了。
+        # (B, T, C) @ (B, C, head_size) -> (B, T, head_size)
+        K = self.key(X)
+        Q = self.query(X)
+        V = self.value(X)
+
+        W = Q @ K.transpose(-2, -1) * c ** -0.5  # (B, T, T)
+        # 解码时，block_size是t的上限，不要误以为相等。预测其实相当于解码。
+        W = W.masked_fill(self.tril[:t, :t] == 0, float('-inf'))
+        W = F.softmax(W, dim=-1)
+        OUT = W @ V
+        return OUT
+
+
+class MultiHead(nn.Module):
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([AttentionHead(head_size) for _ in range(num_heads)])
+
+    def forward(self, X):  # (B, T, C) -> (B, T, C)
+        return torch.cat([h(X) for h in self.heads], dim=-1)
+
+
+class FeedForward(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd, bias=False),
+            nn.ReLU()
+        )
+
+    def forward(self, X):
+        return self.net(X)
+
+
+class Block(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.sa = MultiHead(4, n_embd // 4)
+        self.ffwd = FeedForward()
+
+    def forward(self, X):
+        X = self.sa(X)
+        X = self.ffwd(X)
+        return X
+
+
 # 模型结构（模型 = 结构 + 参数）
-class BigramLanguageModel(nn.Module):
+class TransformerModel(nn.Module):
 
     def __init__(self):  # vocal_size是上面的全局参数，不需要传入构造函数。
         super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)  # (B, T, vocab_size)->(B, T, n_embd)
         self.pos_embedding_table = nn.Embedding(block_size, n_embd)  # pos和token编码长度必须一致，下面要加起来。
+        self.blocks = nn.Sequential(
+            *[Block() for _ in range(n_blocks)]
+        )
         self.lm_head = nn.Linear(n_embd, vocab_size)  # 这里不再像二元模型时 编码维度==词汇量大小，因为解码不那么容易，需要明确的中间层。
 
     def forward(self, idx, target=None):
         tok_emd = self.token_embedding_table(idx)  # tok_emd.shape = (B, T, C), C = n_embd
-        pos_emd = self.pos_embedding_table(torch.arange(block_size, device=device))  # T = block_size
-        integral_emd = tok_emd + pos_emd  # (T, C) + (B, T, C)
-        logits = self.lm_head(integral_emd)  # logits.shape = (B, T, vocab_size)
+        pos_emd = self.pos_embedding_table(
+            torch.arange(idx.shape[1], device=device))  # 不能写block_size, 它是T=idx.shape[1]的上限
+        X = tok_emd + pos_emd  # (T, C) + (B, T, C)
+        X = self.blocks(X)
+        logits = self.lm_head(X)  # (B, T, n_embd)->(B, T, vocab_size)
 
         if target is None:
             loss = None
@@ -90,7 +161,8 @@ class BigramLanguageModel(nn.Module):
 
     def generate(self, idx, max_tokens):
         for _ in range(max_tokens):
-            logits, loss = self(idx)
+            content = idx[:, -block_size:]  # 有位置嵌入表后，输入的长度必须限制，否则查表会越界。ps:刚好可以输入最大值的上下文长度block，而不是block-1
+            logits, loss = self(content)
             logits = logits[:, -1, :]
             probs = F.softmax(logits, dim=-1)
             idx_next = torch.multinomial(probs, num_samples=1)
@@ -98,7 +170,7 @@ class BigramLanguageModel(nn.Module):
         return idx
 
 
-model = BigramLanguageModel().to(device)  # 模型实例
+model = TransformerModel().to(device)  # 模型实例
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)  # 优化器实例（梯度下降策略，不计算梯度，依赖loss.backward()计算的梯度值）
 
 # 训练
@@ -116,5 +188,5 @@ print(loss)
 # 预测
 print(decoder(model.generate(torch.zeros((1, 1), dtype=torch.long, device=device), 500)[0].tolist()))
 # 仅保存模型参数
-torch.save(model.state_dict(), "bigramModel.pth")
-print("模型保存成功")
+# torch.save(model.state_dict(), "transformerModel.pth")
+# print("模型保存成功")
